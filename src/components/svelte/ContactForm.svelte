4aca13648927f276a17e6c4eef394888f18cc6cf
Aaron Conway <aaron@kaseyhomeloans.com>
2024-02-10 18:29:55 -0700

    removed script from astronav

---
+++ src/components/svelte/ContactForm.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  let success: string;
+
+  async function submit(e: SubmitEvent) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    const response = await fetch('/api/feedback', {
+      method: 'POST',
+      body: formData,
+    });
+    const data = await response.json();
+    success = data.ok;
+  }
+</script>
+
+<div class="form-wrapper">
+  <form on:submit={submit} class="form">
+    <label>
+      Name
+      <input type="text" id="name" name="name" required />
+    </label>
+    <label>
+      Phone
+      <input type="text" id="phone" name="phone" required />
+    </label>
+    <label>
+      Email
+      <input type="text" id="email" name="email" required />
+    </label>
+    <label>
+      Message
+      <textarea id="message" name="message" required />
+    </label>
+    <button>Send</button>
+    {#if success}
+      <p>yay it was a success</p>
+    {/if}
+  </form>
+</div>
+
+<style lang="scss">
+  .form {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: var(--space-md);
+
+    label {
+      display: flex;
+      gap: var(--space-xs);
+      flex-direction: column;
+
+      input {
+        border: 1px solid var(--mid-grey);
+        border-radius: 4px;
+      }
+
+      [id='message'] {
+        text-align: left;
+        border: 1px solid var(--mid-grey);
+        border-radius: 4px;
+        justify-content: start;
+        height: 5rem;
+      }
+    }
+  }
+</style>
---
+++ src/pages/api/contact.ts
@@ -0,0 +1,24 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request }) => {
+    const data = await request.formData();
+    const name = data.get("name");
+
+    // Validate the data - you'll probably want to do more than this
+
+    if (!name) {
+        return new Response(
+            JSON.stringify({
+                message: "Missing required fields",
+            }),
+            { status: 400 }
+        );
+    }
+    // Do something with the data, then return a success response
+    return new Response(
+        JSON.stringify({
+            message: "Success!"
+        }),
+        { status: 200 }
+    );
+};
